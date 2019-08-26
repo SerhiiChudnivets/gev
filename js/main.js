c8cef741f613c103edf6b14b257dc0c77d54beb5
User <user@Users-MacBook-Pro.local>
2019-08-26 20:35:58 +0300

    mailer

--- js/main.js
+++ js/main.js
@@ -60,26 +60,26 @@ function counter(data,element) {
     }, timeout)
 }
 
-$(document).ready(function() {
-
-    //E-mail Ajax Send
-    $("form").submit(function() { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function() {
-            // alert("Thank you!");
-            setTimeout(function() {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
-
-});
+// $(document).ready(function() {
+//
+//     //E-mail Ajax Send
+//     $("form").submit(function() { //Change
+//         var th = $(this);
+//         $.ajax({
+//             type: "POST",
+//             url: "mail.php", //Change
+//             data: th.serialize()
+//         }).done(function() {
+//             // alert("Thank you!");
+//             setTimeout(function() {
+//                 // Done Functions
+//                 th.trigger("reset");
+//             }, 1000);
+//         });
+//         return false;
+//     });
+//
+// });
 
 $(document).ready(function(){
     $(".menu, .mobile__menu").on("click","a", function (event) {
