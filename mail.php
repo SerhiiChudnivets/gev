7f0ebf47baa9ed76267ba8f00764029599f674d5
ivan <ischenko.ivan.ivanovich@gmail.com>
2019-08-29 13:47:49 +0300

    ч

--- mail.php
+++ mail.php
@@ -1,24 +1,19 @@
-<?php 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'ffpr2015@gmail.com';
-$mail->Password = 'Posmishka1999';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
-$mail->setFrom('ffpr2015@gmail.com');
-$mail->addAddress('serhiichudnivets@gmail.com');
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = $name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ';
-$mail->AltBody = '';
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    header('location: thank-you.html');
+<?php if(isset($_POST) && isset($_POST['user_name']) && isset($_POST['user_phone'])){
+
+    $recepients = 'gevisdigitalmarketing@gmail.com';
+    $name = $_POST['user_name'];
+    $phone = $_POST['user_phone'];
+    $subject = 'New Lead';
+    $message = '<table><tr><td>Lead:</td><td>Name</td><td>Phone</td></tr><tr><td>#</td><td>'.$name.'</td><td>'.$phone.'</td></tr></table>';
+    $headers  = "From: Landing < gevisdigitalmarketing@gmail.com >\n";
+    $headers .= 'X-Mailer: PHP/' . phpversion();
+    $headers .= "X-Priority: 1\n"; // Urgent message!
+    $headers .= "CC: gevisdigitalmarketing@gmail.com\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $result = mail($recepients, $subject, $message, $headers);
+}else{
+    die('sorry');
 }
+
 ?>
